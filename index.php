8488282408ca5d8dbd4d06840aae1b42e10a51af
Seiji.Kitamura <seiji.kitamura@gmail.com>
2015-02-07 10:26:43 +0900

    index.php変更

--- index.php
+++ index.php
@@ -1,35 +1,77 @@
+<?php require_once("php/html.function.php");?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+  <title>Rainsデータ更新</title>
+  <meta name="viewport" content="width=device-width,user-scalable=no,maximum-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/all.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+  <script src="js/bxslider/jquery.bxslider.min.js"></script>
+  <link  href="js/bxslider/jquery.bxslider.css" rel="stylesheet">
+ </head>
+ <body>
+  <div id="wrap">
+
+   <div id="header">
+
+    <div class="logoSpace">
+     <p>地域の不動産、お任せください</p>
+     <?php htmlLogo(); ?>
+    </div><!-- div class="logoSpace" -->
+    
+    <div class="eventSpace">
+    </div><!-- div class="eventSpace" -->
+
+    <div class="storeInfo">
+     <ul>
+      <li><?php echo CORPNAME; ?></li>
+      <li><?php echo CORPADDRESS; ?></li>
+      <li>TEL:<?php echo CORPTEL; ?>
+          FAX:<?php echo CORPFAX; ?></li>
+     </ul>
+    </div><!-- div class="storeinfo" -->
+   
+    <div class="clr"></div>
+
+    <div class="naviBar">
+     <ul>
+      <li><a href="index.php">ホーム</a></li>
+      <li><a href="#">賃貸</a></li>
+      <li><a href="#">売買</a></li>
+      <li><a href="#">事業用</a></li>
+      <li><a href="#">Q&A</a></li>
+      <li><a href="#">家主様</a></li>
+      <li><a href="#">会社案内</a></li>
+      <li><a href="#">お問い合せ</a></li>
+     </ul>
+    </div><!-- div class="naviBar" -->
+
+    <div class="imageSpace">
+     <!-- トップイメージ(大)はここに -->
+    </div><!-- div class="imageSpace" -->
+
+   </div><!--div id="header" -->
+
+   <div id="leftside">
+   </div><!--div id="leftside" -->
+   
+   <div id="main">
 <?php
-require_once("php/parts.class.php");
+$data=viewEntry(1);
+echo "<pre>";
+print_r($data);
+echo "</pre>";
 ?>
-<?php
-$db=new PARTS();
-//Headerをゲット
-$db->getHeader();
-echo $db->rainsdata["header"];
-?>
-  <div id="topbanner">
-   <div class="divimg">
-    <img src="img/00053.JPG">
-   </div>
-   <p id="overimg">ご希望の不動産全力でお探しいたします。</p>
-   <p id="comment1">物件をお探しのお客様はこちらからどうぞ↓</p>
-   <ul class="ulbignavi">
-    <li><a href="#">賃 貸</a></li>
-    <li><a href="#">売 買</a></li>
-    <li><a href="#">事業用</a></li>
-   </ul>
-   <div class="clr"></div>
-   <hr>
-  </div>
-  <div class="clr"></div>
-  <div class="newtintai">
-<?php
-$db->smallparts();
-?>
-  </div>
-  <ul>
-   <li><a href="list.php">一覧(お客様用)</a></li>
-   <li><a href="uploadrains.php">データ更新</a></li>
-  </ul>
+   </div><!-- div id="main" -->
+   
+   <div id="rightside">
+   </div><!-- div id="rightside" -->
+
+   <div id="footer">
+   </div><!-- div id="footer" -->
+   
+  </div><!-- div id="wrap" -->
  </body>
 </html>
