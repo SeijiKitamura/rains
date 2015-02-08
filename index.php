6f28d7075a493783960df7ac37ccd6545b0f2feb
Seiji.Kitamura <seiji.kitamura@gmail.com>
2015-02-08 21:37:08 +0900

    index.php 修正
    html.function.phpを使用して表示するように変更

--- index.php
+++ index.php
@@ -1,82 +1,8 @@
 <?php require_once("php/html.function.php");?>
-<!DOCTYPE html>
-<html>
- <head>
-  <meta charset="UTF-8">
-  <title>Rainsデータ更新</title>
-  <meta name="viewport" content="width=device-width,user-scalable=no,maximum-scale=1">
-  <link rel="stylesheet" type="text/css" href="css/all.css">
-  <link rel="stylesheet" type="text/css" href="css/index.css">
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-  <script src="js/bxslider/jquery.bxslider.min.js"></script>
-  <link  href="js/bxslider/jquery.bxslider.css" rel="stylesheet">
- </head>
- <body>
-  <div id="wrap">
-
-   <div id="header">
-
-    <hr>
-
-    <div class="logoSpace">
-     <p>地域の不動産、お任せください</p>
-     <?php htmlLogo(); ?>
-    </div><!-- div class="logoSpace" -->
-    
-    <div class="eventSpace">
-    </div><!-- div class="eventSpace" -->
-
-    <div class="storeInfo">
-     <ul>
-      <li><?php echo CORPNAME; ?></li>
-      <li><?php echo CORPADDRESS; ?></li>
-      <li>TEL:<span class="tel"><?php echo CORPTEL; ?></span><li>
-      <li>FAX:<?php echo CORPFAX; ?></li>
-     </ul>
-    </div><!-- div class="storeinfo" -->
-   
-    <div class="clr"></div>
-    <hr>
-
-    <div class="naviBar">
-     <ul>
-      <li><a href="index.php">ホーム</a></li>
-      <li><a href="#">賃貸</a></li>
-      <li><a href="#">売買</a></li>
-      <li><a href="#">事業用</a></li>
-      <li><a href="#">Q&A</a></li>
-      <li><a href="#">家主様</a></li>
-      <li><a href="#">会社案内</a></li>
-      <li><a href="#">お問い合せ</a></li>
-     </ul>
-     <div class="clr"></div>
-    </div><!-- div class="naviBar" -->
-
-
-
-    <div class="imageSpace">
-     <img src="./img/00053.JPG">
-    </div><!-- div class="imageSpace" -->
-
-    <div class="commentSpace">
-     <p>物件をお探しの方、こちらからどうぞ！</p>
-
-     <div class="bigNavi">
-      <ul>
-       <li><a href="#">賃貸</a></li>
-       <li><a href="#">売買</a></li>
-       <li><a href="#">事業用</a></li>
-      </ul>
-      <div class="clr"></div>
-     </div><!-- div class="Bignavi" -->
-
-    </div><!-- div class="commentSpace" -->
-
-    <div class="clr"></div>
-
-    <hr>
-    
-   </div><!--div id="header" -->
+<?php
+htmlHeader($NAVIARY["index.php"]["title"]);
+htmlTopImage();
+?>
 
    <div id="leftside">
    </div><!--div id="leftside" -->
@@ -87,31 +13,6 @@ $data=viewEntry(1);
 if(is_array($data)){
  partsEstateList($data);
 }
-
-
-//$data=viewEntry(2);
-//if(is_array($data)){
-// partsEstateList($data);
-//}
-//
-//$data=viewEntry(3);
-//if(is_array($data)){
-// partsEstateList($data);
-//}
-//
-//$data=viewEntry(4);
-//if(is_array($data)){
-// partsEstateList($data);
-//}
-//
-//
-//$data=viewEntry(5);
-//if(is_array($data)){
-// partsEstateList($data);
-//}
-echo "<pre>";
-print_r($data);
-echo "</pre>";
 ?>
    </div><!-- div id="main" -->
    
