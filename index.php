1b8f61afadca1c6ce2f03eeb1888146bdd43fa22
Seiji.Kitamura <seiji.kitamura@gmail.com>
2015-02-08 18:06:21 +0900

    index.php 修正

--- index.php
+++ index.php
@@ -16,6 +16,8 @@
 
    <div id="header">
 
+    <hr>
+
     <div class="logoSpace">
      <p>地域の不動産、お任せください</p>
      <?php htmlLogo(); ?>
@@ -28,12 +30,13 @@
      <ul>
       <li><?php echo CORPNAME; ?></li>
       <li><?php echo CORPADDRESS; ?></li>
-      <li>TEL:<?php echo CORPTEL; ?>
-          FAX:<?php echo CORPFAX; ?></li>
+      <li>TEL:<span class="tel"><?php echo CORPTEL; ?></span><li>
+      <li>FAX:<?php echo CORPFAX; ?></li>
      </ul>
     </div><!-- div class="storeinfo" -->
    
     <div class="clr"></div>
+    <hr>
 
     <div class="naviBar">
      <ul>
@@ -46,12 +49,33 @@
       <li><a href="#">会社案内</a></li>
       <li><a href="#">お問い合せ</a></li>
      </ul>
+     <div class="clr"></div>
     </div><!-- div class="naviBar" -->
 
+
+
     <div class="imageSpace">
-     <!-- トップイメージ(大)はここに -->
+     <img src="./img/00053.JPG">
     </div><!-- div class="imageSpace" -->
 
+    <div class="commentSpace">
+     <p>物件をお探しの方、こちらからどうぞ！</p>
+
+     <div class="bigNavi">
+      <ul>
+       <li><a href="#">賃貸</a></li>
+       <li><a href="#">売買</a></li>
+       <li><a href="#">事業用</a></li>
+      </ul>
+      <div class="clr"></div>
+     </div><!-- div class="Bignavi" -->
+
+    </div><!-- div class="commentSpace" -->
+
+    <div class="clr"></div>
+
+    <hr>
+    
    </div><!--div id="header" -->
 
    <div id="leftside">
@@ -60,6 +84,9 @@
    <div id="main">
 <?php
 $data=viewEntry(1);
+partsEstateList($data);
+partsEstateList($data);
+
 echo "<pre>";
 print_r($data);
 echo "</pre>";
