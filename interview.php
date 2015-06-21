6e547b6c8ff6dcb11456ff4a518c2bc502476164
Seiji.Kitamura <seiji.kitamura@gmail.com>
2015-06-21 19:15:39 +0900

    modified:   interview.php
    レイアウト修正

--- interview.php
+++ interview.php
@@ -1,24 +1,61 @@
 <?php
 require_once("php/html.function.php");
-htmlHeader("インタビュー");
+htmlHeader("会社概要");
 ?>
    <div id="contents_wrap">
     <div id="contents_inner">
      <div id="divintvw">
-      <h2>キタムラ地所で働く「北村　哲彦」さんにインタビューしました。</h2>
+
+      <!--トップイメージ-->
+      <div class="intvwTopImage">
+       <img src="">
+      </div><!--div class="intvwTopImage"-->
+ 
+      <h2>にインタビューしました。</h2>
+
+      <!-- 写真左のひな形 -->
       <div class="intvwpart">
+
        <div class="Ldivintvwimg">
+        <img src="">
        </div><!--div class="Ldivintvwimg"-->
-       <div class="Rdivintvwimg">
-       </div><!--div class="Rdivintvwimg"-->
-       <div class="inner-part">
-        <h3></h3>
-        <p>
-        </p>
-       </div><!--div class="inner-part"-->
-       <div class="clr"></div>
+
+       <div class="Linner-part">
+       </div><!--div class="Linner-part"-->
+
+       <h3>質問事項</h3>
+       <p>
+          回答
+       </p>
+
       </div><!--div class="intvwpart"-->
-     </div><!--div id="divintvw"-->
+
+      <!-- 写真右のひな形 -->
+      <div class="intvwpart">
+
+       <div class="Rdivintvwimg">
+        <img src="">
+       </div><!--div class="Rdivintvwimg"-->
+
+       <div class="Rinner-part">
+       </div><!--div class="Rinner-part"-->
+
+       <h3>質問事項</h3>
+       <p>
+          回答
+       </p>
+
+      </div><!--div class="intvwpart"-->
+
+      <div class="contactItemBox">
+      <h4><?php echo CORPNAME;?>へのお問い合せは下記までにご連絡くださいませ。</h4>
+       <p>
+        <img src="img/tel.png">
+        <a href="mailto:<?php echo MAILADDRESS;?>>"><img src="img/mailto.png"></a>
+       </p>
+      </div><!--div id="contactItemBox"-->
+
+     </div><!--div id="divannai"-->
     </div><!--div id="contents_inner"-->
    </div><!--div id="contents_wrap"-->
 
