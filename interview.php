d81d3c07cd800919a2bc8eabf2c006ec6d52afb8
Seiji.Kitamura <seiji.kitamura@gmail.com>
2015-06-21 18:02:53 +0900

    interview.php 作成
    インタビュー用ページひな形作成

---
+++ interview.php
@@ -0,0 +1,40 @@
+<?php
+require_once("php/html.function.php");
+htmlHeader("インタビュー");
+?>
+   <div id="contents_wrap">
+    <div id="contents_inner">
+     <div id="divintvw">
+      <h2>キタムラ地所で働く「北村　哲彦」さんにインタビューしました。</h2>
+      <div class="intvwpart">
+       <div class="Ldivintvwimg">
+       </div><!--div class="Ldivintvwimg"-->
+       <div class="Rdivintvwimg">
+       </div><!--div class="Rdivintvwimg"-->
+       <div class="inner-part">
+        <h3></h3>
+        <p>
+        </p>
+       </div><!--div class="inner-part"-->
+       <div class="clr"></div>
+      </div><!--div class="intvwpart"-->
+     </div><!--div id="divintvw"-->
+    </div><!--div id="contents_inner"-->
+   </div><!--div id="contents_wrap"-->
+
+   <div id="footer">
+<?php
+htmlFooter();
+?>
+   </div><!-- div id="footer" -->
+  </div><!-- div id="wrap" -->
+ </body>
+ <script>
+$(function(){
+ navi();
+});
+ </script>
+</html>
+
+
+
